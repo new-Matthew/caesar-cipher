6416d19b3b76416e5282546f034e7c38097b8115
matheus <matheusrodrig16@gmail.com>
2024-11-20 19:14:53 -0300

    feat: Encrypt function

---
+++ main.py
@@ -0,0 +1,16 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+original_text = input("Escreva o texto que deseja encriptar: ")
+shift_amount = int(input("Digite o número de deslocamentos: "))
+
+print("Bem vindo à CIFRA DE CÉSAR!")
+
+def encrypt(text, shift):
+    text_encypted = ""
+    for letter in text:
+        shifted_position = alphabet.index(letter) + shift
+        text_encypted += alphabet[shifted_position]
+
+    return print(text_encypted)
+
+encrypt(text=original_text, shift=shift_amount)
