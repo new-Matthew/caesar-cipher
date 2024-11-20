5caa9491633b500058341a6f0fdd1df4c296858a
matheus <matheusrodrig16@gmail.com>
2024-11-20 19:20:17 -0300

    fix: index out of range

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 original_text = input("Escreva o texto que deseja encriptar: ")
 shift_amount = int(input("Digite o número de deslocamentos: "))
-
+size_alphabet = len(alphabet)
 print("Bem vindo à CIFRA DE CÉSAR!")
 
 def encrypt(text, shift):
     text_encypted = ""
     for letter in text:
-        shifted_position = alphabet.index(letter) + shift
+        shifted_position = (alphabet.index(letter) + shift) % size_alphabet
         text_encypted += alphabet[shifted_position]
 
     return print(text_encypted)
