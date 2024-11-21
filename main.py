263e31ec47aedeb14fbe713678297068e1b19ec9
matheus <matheusrodrig16@gmail.com>
2024-11-21 11:59:47 -0300

    refactor: Changes in logic of caesar function and handling of non alphabet inputs

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = int(input("Digite '1' para encriptar ou '2' para desencriptar: "))
-original_text = input("Escreva o texto que deseja encriptar: ")
-shift_amount = int(input("Digite o número de deslocamentos: "))
-size_alphabet = len(alphabet)
 print("Bem vindo à CIFRA DE CÉSAR!")
 
 def caesar(text, shift, encode_or_decode):
     text_encrypted = ""
-    for letter in text:
-        shifted_position = (alphabet.index(letter) + shift) % size_alphabet
-        text_encrypted += alphabet[shifted_position]
+    non_alpha_chars = ""
 
-    text_decrypted = ""
-    for letter in text_encrypted:
-        shifted_position = (alphabet.index(letter) - shift) % size_alphabet
-        text_decrypted += alphabet[shifted_position]
+    for letter in text:
+        if letter in alphabet:
+            original_index = alphabet.index(letter)
+            if encode_or_decode == 1:
+                shifted_position = (original_index + shift) % size_alphabet
+            elif encode_or_decode ==2:
+                shifted_position = (original_index - shift) % size_alphabet
+            text_encrypted += alphabet[shifted_position]
+        else:
+            non_alpha_chars += letter
+
+    text_encrypted += non_alpha_chars
 
     if encode_or_decode == 1:
-        print(f"o texto encriptado é: {text_encrypted}")
+        print(f"O texto encriptado é: {text_encrypted}")
     elif encode_or_decode == 2:
-        print(f"o texto encriptado é: {text_encrypted}")
-        print(f"o texto decriptado é: {text_decrypted}")
+        print(f"O texto decriptado é: {text_encrypted}")
     else:
-        print(f"insira um valor válido!") 
+        print("Insira um valor válido!")
+
+
+
+
+direction = int(input("Digite '1' para encriptar ou '2' para desencriptar: "))
+original_text = input("Escreva o texto que deseja encriptar: ")
+shift_amount = int(input("Digite o número de deslocamentos: "))
+size_alphabet = len(alphabet)
 
 caesar(text=original_text, shift=shift_amount, encode_or_decode=direction)
 
